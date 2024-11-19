c542579b8df3f6511a55f530df9a3a8cc413f600
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-19 10:34:58 -0300

    :sparkles: feat: QureoDoar.jsx

---
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -0,0 +1,44 @@
+import Book from "../../assets/images/book.png";
+import D from "./QueroDoar.module.scss";
+
+export default function QueroDoar() {
+  return (
+    <section className={D.doados}>
+      <p>
+        Por favor, preencha o formulário com suas informações e as informações
+        do Livro
+      </p>
+
+      <div className={D.boxForm}>
+        <div className={D.bookTitle}>
+          <img src={Book} alt="Livro aberto" />
+          <h2>Informações do Livro</h2>
+        </div>
+
+        <form className={D.formDoados}>
+          <div>
+            <input type="text" id="title" />
+            <label htmlFor="title">Titulo</label>
+          </div>
+
+          <div>
+            <input type="text" id="category" />
+            <label htmlFor="category">Categoria</label>
+          </div>
+
+          <div>
+            <input type="text" id="autor" />
+            <label htmlFor="autor">Autor</label>
+          </div>
+
+          <div>
+            <input type="text" id="upload" />
+            <label htmlFor="upload">Link da Imagem</label>
+          </div>
+
+          <input type="submit" value="Doar" />
+        </form>
+      </div>
+    </section>
+  );
+}
